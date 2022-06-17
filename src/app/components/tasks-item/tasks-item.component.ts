fb13c501811c470aaf1146cd4c74ff12946a1b09
Gabriela <97000496+gabirrulos248@users.noreply.github.com>
2022-06-17 15:19:31 -0300

    Añadiendo tareas desde el tasks-item part1

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Task} from '../../Task'
+import {TASKS} from '../../mock-tasklist'
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
   styleUrls: ['./tasks-item.component.css']
 })
 export class TasksItemComponent implements OnInit {
-
+@Input() task: Task = TASKS[0]
   constructor() { }
 
   ngOnInit(): void {
