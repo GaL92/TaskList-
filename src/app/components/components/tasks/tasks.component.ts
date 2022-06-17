5d3c1e6cd5a890dbf7dbde1e36ab1b80d33f2d99
Gabriela <97000496+gabirrulos248@users.noreply.github.com>
2022-06-17 05:30:39 -0300

    error en la propiedad texto de tasks.component.ts'

--- src/app/components/components/tasks/tasks.component.ts
+++ src/app/components/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Task} from '../../../Task';
 import {TASKS} from '../../../mock-tasklist';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
