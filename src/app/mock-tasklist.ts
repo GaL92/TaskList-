07f6d7b03d1fb300aebc30e7e193db6a1044799f
Gabriela <97000496+gabirrulos248@users.noreply.github.com>
2022-06-17 02:28:40 -0300

    mock de Tareas y el interface de las mismas

---
+++ src/app/mock-tasklist.ts
@@ -0,0 +1,39 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    }
+
+    {
+
+        id: 2,
+        text: "Hacer comprar para la cena",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    }
+
+    {
+
+        id: 3,
+        text: "Ivestigar sobre Bootstrap",
+        day: 'Agosto 5 a las 19:00',
+        reminder: false
+    }
+
+    {
+
+        id: 4,
+        text: "Leer un libro favorito",
+        day: 'Agosto 5 a las 21:00',
+        reminder: false
+    }
+
+
+]
+
+
