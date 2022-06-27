d7acea9222db4fadcc818d264488cc9d068a8a8a
Gabriela <97000496+gabirrulos248@users.noreply.github.com>
2022-06-26 22:12:21 -0300

    Error Delete Task

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {Task} from "../Task"
 import {TASKS} from "../mock-tasklist"
@@ -6,12 +7,20 @@ import {TASKS} from "../mock-tasklist"
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(
 
-  getTasks(): Observable< Task[]> {
+    private http:HttpClient
+  ) { }
 
-    const tasks = of(TASKS);
-    return tasks
+  getTasks(): Observable<Task[]> {
+
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const url = '${this.apiUrl}/${task.id}'
+    return this.http.delete<Task>(url)
   }
 }
